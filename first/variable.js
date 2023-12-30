f6287bf460ecfbf996de5ed2b240098f2e749d33
MD HIDAYAT <55959880+Hidayat78@users.noreply.github.com>
2023-12-30 18:42:03 +0000

    data types

--- first/variable.js
+++ first/variable.js
@@ -15,3 +15,25 @@ function varTest() {
     }
     console.log(x); // 1
   }
+  const accountId = 144553
+let accountEmail = "h@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hidayat@x.com"
+accountPassword = "007007007"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
