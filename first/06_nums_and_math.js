0fc2c2d55b879dfb868edf50abb663e50404cc0b
MD HIDAYAT <55959880+Hidayat78@users.noreply.github.com>
2024-01-04 15:57:43 +0000

    splice or slice

--- first/06_nums_and_math.js
+++ first/06_nums_and_math.js
@@ -30,4 +30,5 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
---
+++ first2/01_arrays.js
@@ -0,0 +1,31 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj", "Per-Man"]
+// node first2/01_arrays.js path for run
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+//Array method
+
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+// console.log(myArr.indexOf(2));
+const newArr = myArr.join()
+console.log(newArr);
+console.log(myArr);
+
+/ slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
