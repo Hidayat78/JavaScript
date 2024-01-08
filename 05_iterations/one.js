d373b42737f4332529d2cd1386a691378166c2da
MD HIDAYAT <55959880+Hidayat78@users.noreply.github.com>
2024-01-08 08:35:29 +0000

    loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,5 +1,34 @@
 // for loop
 for(let i=0; i< 10; i++){
     const ele= i;
-    console.log(ele)    
+    // console.log(ele)    
+}
+
+for(let i=1; i<10; i++){
+    // console.log(`Ouer Loops value ${i}`)
+    for(let j=1; j<=10; j++){
+        // console.log(`Inner  Loops ${j} value ${i}`)
+        // console.log(i + '*' + j + ' = ' + i*j );
+    }
+}
+let myArray= ['flash', 'Batman', 'SuperMan'];
+for(let i=0; i<myArray.length; i++){
+    const ele = myArray[i]
+    // console.log(ele)
+}
+
+// break and continue
+// for(let i =0; i<= 20; i++){
+//     if(i==5){
+//         console.log("Dected 5")
+//         break
+//     }
+//     console.log(`value of i is ${i}`)
+// }
+for(let i =0; i<= 20; i++){
+    if(i==5){
+        console.log("Dected 5")
+        continue
+    }
+    console.log(`value of i is ${i}`)
 }
